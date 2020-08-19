29c535afd4edbaade51f1953e502ea0c37c995ba
Gustavo Felipe <gustavo.felipebc@gmail.com>
2020-08-19 13:29:37 -0300

    correçao na busca na page TeacherForm

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -37,7 +37,7 @@ export default class ClassesController{
 
             })
             .where('classes.subject', '=', subject)
-            .join('users', 'classes.user_id', '=', 'user_id')
+            .join('users', 'classes.user_id', '=', 'users.id')
             .select(['classes.*', 'users.*'])
             
         return response.json(classes);
